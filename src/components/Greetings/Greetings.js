ed5545b99f8445a767c96411435eb18968281d89
Reynold Figueroa <reynold0801@outlook.com>
2021-09-23 20:57:57 -0400

    Greetings layout

--- src/components/Greetings/Greetings.js
+++ src/components/Greetings/Greetings.js
@@ -7,17 +7,23 @@ export class Greetings extends Component {
     return (
       <section className="greetings">
         <div className="greetings__about">
+          <div className="about__welcome">Hello, Im Reynold</div>
           <div className="about__text">
-            <div className="text__welcome">Welcome</div>
-            <div className="text__about">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              commodi excepturi vel est eum quis?
-            </div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
+            possimus tempora quasi nobis delectus amet explicabo, iure ad fugiat
+            exercitationem. Harum odio deleniti sit, quis amet explicabo
+            consequuntur a ab facere voluptas iure sed, libero, adipisci quae
+            laudantium perspiciatis? Soluta ea sint voluptate aspernatur
+            pariatur magnam enim provident consectetur vel.
           </div>
+
           <div className="about__tags">
             <div className="tag-pill">Pill 1</div>
             <div className="tag-pill">Pill 2</div>
             <div className="tag-pill">Pill 3</div>
+            <div className="tag-pill">Pill 4</div>
+            <div className="tag-pill">Pill 5</div>
+            <div className="tag-pill">Pill 6</div>
           </div>
           <div className="about__cv">
             <button>CV</button>
